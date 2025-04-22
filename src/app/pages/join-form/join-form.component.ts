78aac8d093fefb3e2555e78ef8f7ebce39108906
surajambrale <surajambrale9003@gmail.com>
2025-04-22 16:42:58 +0530

    Added form component and backend

---
+++ src/app/pages/join-form/join-form.component.ts
@@ -0,0 +1,38 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-join-form',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './join-form.component.html',
+  styleUrl: './join-form.component.scss'
+})
+export class JoinFormComponent {
+  @Output() onClose = new EventEmitter<void>();
+
+  formData ={
+    name: '',
+    phone:'',
+    message:''
+  };
+
+  constructor(private http: HttpClient){}
+
+  submitForm() {
+    this.http.post('http://localhost:3000/enquiries', this.formData).subscribe(() => {
+      alert("Form submitted successfully!");
+      this.close();
+    }, error => {
+      alert("Error occurred!");
+      console.error(error);
+    });
+  }
+
+  close(){
+    this.onClose.emit();
+  }
+
+}
