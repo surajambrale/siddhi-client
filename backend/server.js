dff5e2374a4baacd0308f3cb011035b977f9489f
surajambrale <surajambrale9003@gmail.com>
2025-04-30 17:29:30 +0530

    server.js file me option method add kiya he

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(cors({
       callback(new Error('Not allowed by CORS'));
     }
   },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true
 }));
 
