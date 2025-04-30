7ec3d8bcee8b01abf73572f7607bdc6b07b93e19
surajambrale <surajambrale9003@gmail.com>
2025-04-30 17:38:56 +0530

    join form ts file me link add kerni padti he ..render diploy hone ke bad link milti he ...vo link and slash kerke db ke folder ka nam add kerna he

--- src/app/pages/join-form/join-form.component.ts
+++ src/app/pages/join-form/join-form.component.ts
@@ -22,7 +22,7 @@ export class JoinFormComponent {
   constructor(private http: HttpClient){}
 
   submitForm() {
-    this.http.post('https://gym-website-b1kh.onrender.com/enquiries', this.formData).subscribe(() => {
+    this.http.post('https://gym-website-repo.onrender.com/enquiries', this.formData).subscribe(() => {
       alert("Form submitted successfully!");
       this.close();
     }, error => {
