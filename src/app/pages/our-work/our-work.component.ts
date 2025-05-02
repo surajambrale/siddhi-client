d4190ebaa6e97baeb23e0bdc6b453d4490b261a3
surajambrale <surajambrale9003@gmail.com>
2025-05-02 12:36:22 +0530

    added client transformation imahges

--- src/app/pages/our-work/our-work.component.ts
+++ src/app/pages/our-work/our-work.component.ts
@@ -11,21 +11,39 @@ import { CommonModule } from '@angular/common';
 export class OurWorkComponent {
   works = [
     {
-      before:  '/assets/images/before.jpg',
-      after: '/assets/images/after.jpg',
+      before:  '/assets/images/shubhambefore.jpg',
+      after: '/assets/images/shubhamafter.jpg',
       name: 'Client 1',
       description: 'Lost 12kg in 3 months with our fat loss plan.'
     },
     {
-      before:  '/assets/images/before.jpg',
-      after: '/assets/images/after.jpg',
+      before:  '/assets/images/fatsabefore.jpg',
+      after: '/assets/images/fatsaafter.jpg',
       name: 'Client 2',
+      description: 'Lost 12kg in 3 months with our fat loss plan.'
+    },
+    {
+      before:  '/assets/images/pramodbefore.jpg',
+      after: '/assets/images/pramodafter.jpg',
+      name: 'Client 3',
       description: 'Gained 6kg of muscle in 8 weeks.'
     },
     {
-      before:  '/assets/images/before.jpg',
-      after: '/assets/images/after.jpg',
-      name: 'Client 3',
+      before:  '/assets/images/mitulbefore.jpg',
+      after: '/assets/images/mitulafter.jpg',
+      name: 'Client 4',
+      description: 'Improved strength and posture significantly.'
+    },
+    {
+      before:  '/assets/images/ankitabefore.jpg',
+      after: '/assets/images/ankitaafter.jpg',
+      name: 'Client 5',
+      description: 'Improved strength and posture significantly.'
+    },
+    {
+      before:  '/assets/images/surajbefore.jpg',
+      after: '/assets/images/surajafter.jpg',
+      name: 'Client 6',
       description: 'Improved strength and posture significantly.'
     }
   ];
